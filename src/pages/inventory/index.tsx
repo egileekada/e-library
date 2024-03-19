d5e9f340a4e30c9f66fc53325852623a22caea62
egileekada <egileoniso.ekada@gmail.com>
2024-03-19 20:11:49 +0100

    changed inventory picture

--- src/pages/inventory/index.tsx
+++ src/pages/inventory/index.tsx
@@ -14,7 +14,7 @@ function InventoryPage(props: Props) {
         <Flex width={"full"} gap={"6"} pt={"8"} >
             <Flex onClick={()=> navigate("/dashboard/library")} as='button' width={"full"} border={"1px solid #BDBDBD"} bgColor={"#FCFCFC"} px={"4"} py={"6"} rounded={"10px"} flexDir={"column"} >
                 <Flex height={"auto"} py={"4"} w={"full"} gap={"2"}  justifyContent={"center"} alignItems={"center"} > 
-                    <Image alt='equipment' src='/literature.jpeg' rounded={"md"} height={"162px"} width={"auto"} />
+                    <Image alt='equipment' src='/book.jpg' rounded={"md"} height={"162px"} width={"auto"} />
                 </Flex>
                 <Flex pt={"5"} width={"full"} justifyContent={"space-between"} alignItems={"center"} >
                     <Text fontWeight={"700"} fontSize={"18px"} color={"#1E1B39"} >Literature</Text>
@@ -23,7 +23,7 @@ function InventoryPage(props: Props) {
             </Flex>
             <Flex onClick={()=> navigate("/dashboard/inventory/equipments")} as='button' width={"full"} border={"1px solid #BDBDBD"} bgColor={"#FCFCFC"} px={"4"} py={"6"} rounded={"10px"} flexDir={"column"} >
                 <Flex height={"auto"} py={"4"} w={"full"} gap={"2"} justifyContent={"center"} alignItems={"center"} > 
-                    <Image alt='equipment' src='/equipment.jpeg' rounded={"md"} height={"162px"} width={"auto"} />
+                    <Image alt='equipment' src='/chair.jpg' rounded={"md"} height={"162px"} width={"auto"} />
                 </Flex>
                 <Flex pt={"5"} width={"full"} justifyContent={"space-between"} alignItems={"center"} >
                     <Text fontWeight={"700"} fontSize={"18px"} color={"#1E1B39"} >Equipment</Text>
@@ -32,7 +32,7 @@ function InventoryPage(props: Props) {
             </Flex>
             <Flex onClick={()=> navigate("/dashboard/inventory/gadgets")} as='button' width={"full"} border={"1px solid #BDBDBD"} bgColor={"#FCFCFC"} px={"4"} py={"6"} rounded={"10px"} flexDir={"column"} >
                 <Flex height={"auto"} py={"4"} w={"full"} gap={"2"} justifyContent={"center"} alignItems={"center"} > 
-                    <Image alt='equipment' src='/gadgets.jpeg' rounded={"md"} height={"162px"} width={"auto"} />
+                    <Image alt='equipment' src='/laptop.jpeg' rounded={"md"} height={"162px"} width={"auto"} />
                 </Flex>
                 <Flex pt={"5"} width={"full"} justifyContent={"space-between"} alignItems={"center"} >
                 <Text fontWeight={"700"} fontSize={"18px"} color={"#1E1B39"} >Gadgets</Text>
